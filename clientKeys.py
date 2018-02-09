264fd2760e502a4a5ef15830dfff664c1662d956
Carter <heronc95@vt.edu>
2018-02-09 10:27:53 -0500

    modified clientKeys.py file

---
+++ clientKeys.py
@@ -0,0 +1,12 @@
+
+#twitter hashtag that we will be searching for
+HASHTAG = "#ECE4564"
+
+#twitter API information
+TWITTER_CONSUMER_KEY = "Q9GMxmNE3X0WHjNd8O6g4zVO5"
+TWITTER_CONSUMER_SECRET = "5RmFz3HtVhnsm0X7Wv7fFYPZVi6Eb1gLdvM0iALVO9sjEvWBo8"
+TWITTER_ACCESS_TOKEN = "961625544872878085-86aUV4QHTjNHvJZKb4MWnDinYQ7rrdP"
+TWITTER_ACCESS_TOKEN_SECRET = "18i5ClqSiKpyLAJihqrlj4mDrH0NnbqWgdpnVszC7mrCl"
+
+#possibly add a delay here for refreshing how often you will read tweets
+DELAY_TO_GRAB_TWEET = 30.0
