7c99065210537083fceffd260d49d382b536325e
AndrewSwag <bandrew1@vt.edu>
2018-02-10 15:28:16 -0500

    The server code with the question asking and the communication to simpleClient.py (in extra_stuff) is done, just need to do checkpoint prints, and make it talk. But the client should just be able to use the communication function I wrote already to send to the server.

--- server.py
+++ server.py
@@ -14,15 +14,30 @@ QUESTION_NUM = 1
 HASH_NUM = 2
 
 
-def parse_question_message(raw_data):
+def get_ip_address():
+    '''
+    This is used to find the IP address of the PI connected to the network, since the server IP can
+    change whenever, and I don't want to have to change my code.
+    :return:
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+def parse_question_message(raw_data, fernet_key_local):
     '''
     This is the handler for reading messages from the sender. It deserializes the data,
     then loads it into a tuple object for further analyises.
     :param raw_data:
+    :param fernet_key_local: The key to update
     :return: A valid question string if correct message is received, or None if nothing was there
     '''
+
     # first have to load the data from the serialized string back into a tuple
     data_tuple = tuple(pickle.loads(raw_data))
+    #Update the key
+    fernet_key_local[0] = data_tuple[KEY_NUM]
 
     # Check the hash first, by rehashing it
     hash_checker = hashlib.md5()
@@ -31,7 +46,7 @@ def parse_question_message(raw_data):
     if received_hash == data_tuple[HASH_NUM]:
         # Now we can decrypt the message that was sent
         # First get the key all set
-        decrypter = Fernet(data_tuple[KEY_NUM])
+        decrypter = Fernet(fernet_key_local[0])
         # Now decrypt the message
         question_bytes = bytes(decrypter.decrypt(data_tuple[QUESTION_NUM]))
         #Now return the string that was sent
@@ -40,6 +55,31 @@ def parse_question_message(raw_data):
         # Then the hash was not good and should not let it decode
         return None
 
+
+def create_message_packet(string_message, key):
+    """
+    This function will build the tuple of the given message and return it. Based of the spec
+    the tuple has (Encypted Message, Checksum of message), dont need the key because it is
+    the client's
+    :param string_message: The string answer that you want to transmit back to the clients
+    :param key: The key that was sent earlier, and is used to encrypt the message back
+    :return: A tuple of the objects to transmit
+    """
+
+    f = Fernet(key)
+    hasher = hashlib.md5()
+    encrypted_question = f.encrypt(string_message.encode())
+
+    # Get the md5 hash of the question
+    hasher.update(encrypted_question)
+    hash = hasher.digest()
+
+    # This is the tuple of all the packets, and just need to pickle it
+    tuple_to_return = (encrypted_question, hash)
+
+    return tuple_to_return
+
+
 def get_answer_wolfram(user_query):
         '''
         This is used to query wolfram alpha. It returns a string if wolfram actually answered the question
@@ -47,11 +87,13 @@ def get_answer_wolfram(user_query):
         :param user_query:
         :return:
         '''
-        client = wolframalpha.Client(serverKeys.appId)
+
+        appId = "TLRHJQ-Q4XKHKPXL9"
+        client = wolframalpha.Client(appId)
 
         # This is where I ask the user to send me the question
         res = client.query(user_query)
-        if res['@success']:
+        if res['@success'] == 'true':
             try:
                 # Result pod is a dictionary that holds the result, have to get that from a list
                 result_pod = list(res.pods)
@@ -60,6 +102,7 @@ def get_answer_wolfram(user_query):
                 # Now get the plaintext response from the subpod from that with the result, so many dicts
                 text_result = result_pod['subpod']['plaintext']
                 return text_result
+
             except KeyError:
                 print("There was an error in looking up the keys in the subpods");
         else:
@@ -71,26 +114,31 @@ def get_answer_wolfram(user_query):
 
 # Start execution here --------------------------------------------------------------------
 
+# Local variables to use
+fernet_key = [None]
 
-#Setting up the argument parser
+# Setting up the argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--SERVER_PORT", help="This is the port the server is listening on, greater that 1024", type=int)
 parser.add_argument("-b", "--BACKLOG_SIZE", help="This is the backlog size of the socket I believe", type=int)
 parser.add_argument("-z", "--SOCKET_SIZE", help="This is the backlog size of the socket I believe", type=int)
 
-#This gets the arguments from the user, access them through SERVER_PORT, etc
+# This gets the arguments from the user, access them through SERVER_PORT, etc
 args = parser.parse_args()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Get the IP address of the wlan to run on
+address = get_ip_address()
+
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('localhost', args.SERVER_PORT)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+# Listen for incoming connections, with the specified number of connections
+sock.listen(args.BACKLOG_SIZE)
 
 while True:
     # Wait for a connection
@@ -98,30 +146,23 @@ while True:
     connection, client_address = sock.accept()
     try:
         print('connection from', client_address)
-
         #Wait fo receive data on the socket
-        data = connection.recv(1024)
-        print('received {!r}'.format(data))
+        data = connection.recv(args.SOCKET_SIZE)
         # parse the data from the client
-        question = parse_question_message(data)
-        print("The question was: " + question)
+        question = parse_question_message(data, fernet_key)
 
         #Get the answer from wolfram alpha
-        answer = str(get_answer_wolfram(question))
+        answer = get_answer_wolfram(question)
+        if answer != None:
+            # Make the answer payload back to the client here
+            response_tuple = tuple(create_message_packet(answer, fernet_key[0]))
+            # Serialize and send to the client
+            connection.sendall(pickle.dumps(response_tuple))
 
-        print("The answer is : " + answer)
-
-        if data:
-            print('sending data back to the client')
-            connection.sendall(data)
 
 
     finally:
         # Clean up the connection
         connection.close()
-
-
-
-#Start the execution here
 
 
